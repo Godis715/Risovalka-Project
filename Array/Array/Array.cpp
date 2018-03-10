8408a506e72b0604b21cdc8f1ec1a432be23e856
Nikita Rassudov <rassudov99@gmail.com>
2018-03-10 19:52:12 +0300

    Work in progress
    Добавился деструктор, PushBack, PopBack, удвоение capacity в случае чего, насыпали исключений

--- Array/Array/Array.cpp
+++ Array/Array/Array.cpp
@@ -7,6 +7,19 @@ private:
 	T* _storage = nullptr;
 	int _size = 0;
 	int _capacity = 512;
+
+	void doubleCapacity()
+	{
+		auto newStorage = new T[2 * _capacity];
+		for (size_t i = 0; i < _capacity; i++)
+		{
+			newStorage[i] = _storage[i];
+		}
+		delete[] _storage;
+		_storage = newStorage;
+		_capacity *= 2;
+	}
+
 public:
 
 	int getSize() const
@@ -40,17 +53,50 @@ public:
 		}
 	}
 
+	~Array()
+	{
+		delete[] _storage;
+	}
+
 	T& operator[](int index)
 	{
+		if (index < 0 || index >= size)
+		{
+			throw std::out_of_range("Index out of range!");
+		}
 		return _storage[index];
+	}
+
+	void pushBack(T value)
+	{
+		if (_size + 1 > _capacity)
+		{
+			doubleCapacity();
+		}
+		_storage[_size++] = value;
+	}
+
+	T popBack()
+	{
+		if (_size == 0)
+		{
+			throw std::out_of_range("Array is empty");
+		}
+		_size--;
+		return _storage[_size];
 	}
 };
 
 int main()
 {
-	Array<int> arr(513);
-	arr[1] = 9;
-	std::cout << arr[1] << "\n";
+	Array<int> arr;
+	arr.pushBack(1);
+	arr.pushBack(2);
+	arr.pushBack(3);
+	std::cout << arr.popBack() << "\n";
+	std::cout << arr.popBack() << "\n";
+	std::cout << arr.popBack() << "\n";
+	std::cout << "Size = " << arr.getSize() << "\n";
 	system("pause");
 	return 0;
 }
