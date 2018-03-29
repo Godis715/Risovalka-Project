9c3165a91f94a453c1aaee81b792b79657f96465
Unknown <oleshkevich19990802@mail.ru>
2018-03-29 18:16:59 +0300

    требования
    расстояние от точки до прямой. я подумал, что перпендикулярность и паралленость - ерунда и ввел угол между прямыми.

--- ProgrammingBasics/ProgrammingBasics/Include/Requirement.h
+++ ProgrammingBasics/ProgrammingBasics/Include/Requirement.h
@@ -26,10 +26,7 @@ public:
 			// требование должно быть удалено, процесс удаления еще не сделан
 		}
 		Point* point2 = dynamic_cast<Point*>(model.dict.GetCurrent());
-		Vector2 pointVec1 = point1->GetPosition();
-		Vector2 pointVec2 = point2->GetPosition();
-		pointVec1 = pointVec1 - pointVec2;
-		return abs(pointVec1.GetLength() - distance);
+		return abs(point1->GetDistance(point2->GetPosition()) - distance);
 	}
 	void ChangeDistance(double dist) {
 		distance = dist;
@@ -82,5 +79,75 @@ private:
 	const ID& segment;
 	const ID& first;
 	const ID& second;
+};
+
+class RequirementDistanceBetweenPointSegment : public IRequirement
+{
+public:
+	RequirementDistanceBetweenPointSegment(const Segment& segment, const Point& point, double dist) :
+		segment(segment.GetId()),
+		first(point.GetId())
+	{
+		distance = dist;
+	}
+	~RequirementDistanceBetweenPointSegment() {}
+	virtual double error() {
+		Model model;
+		if (!model.dict.find(first)) {
+			// требование должно быть удалено, процесс удаления еще не сделан
+		}
+		Point* point = dynamic_cast<Point*>(model.dict.GetCurrent());
+		if (!model.dict.find(segment)) {
+			// требование должно быть удалено, процесс удаления еще не сделан
+		}
+		Segment* seg = dynamic_cast<Segment*>(model.dict.GetCurrent());
+		return abs(seg->GetDistance(point->GetPosition()) - distance);
+	}
+	void ChangeDistance(double dist) {
+		distance = dist;
+	}
+private:
+	const ID& segment;
+	const ID& first;
+	double distance;
+};
+
+class RequirementAngleBtweenSegments : public IRequirement
+{
+public:
+	RequirementAngleBtweenSegments(const Segment& seg1, const Segment& seg2, double ang) :
+		segment1(seg1.GetId()),
+		segment2(seg2.GetId())
+	{
+		angle = ang;
+	}
+	~RequirementAngleBtweenSegments() {}
+	virtual double error() {
+		Model model;
+		if (!model.dict.find(segment1)) {
+			// требование должно быть удалено, процесс удаления еще не сделан
+		}
+		Segment* seg1 = dynamic_cast<Segment*>(model.dict.GetCurrent());
+		if (!model.dict.find(segment2)) {
+			// требование должно быть удалено, процесс удаления еще не сделан
+		}
+		Segment* seg2 = dynamic_cast<Segment*>(model.dict.GetCurrent());
+		Vector2 vec1 = seg1->GetPoint2_pos() - seg1->GetPoint1_pos();
+		Vector2 vec2 = seg2->GetPoint2_pos() - seg2->GetPoint1_pos();
+		double answer = Vector2::Dot(vec1, vec2);
+		double PI_d2 = 3.14159265 / 2;
+		if (answer > PI_d2) {
+			answer = PI_d2 * 2 - answer;
+		}
+		// умножение нужно потому что для большого угла разность будет слишком малой
+		return abs(answer - angle) * PI_d2 / 2;
+	}
+	void ChangeAngle(double ang) {
+		angle = ang;
+	}
+private:
+	const ID& segment1;
+	const ID& segment2;
+	double angle;
 };
 #endif // REQUIREMENT_H
