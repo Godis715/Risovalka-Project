24780b20661e82b813b67153f76297a14d7fae51
Unknown <oleshkevich19990802@mail.ru>
2018-06-28 20:06:40 +0300

    cleanMaster

--- ProgrammingBasics/ProgrammingBasics/Include/Requirement.h
+++
@@ -1,9 +0,0 @@
-#ifndef REQUIREMENT_H
-#define REQUIREMENT_H
-
-class IRequirement{
-public :
-	virtual double error() = 0;
-};
-
-#endif // REQUIREMENT_H
--- ProgrammingBasics/ProgrammingBasics/Include/hidden/IView(hidden).h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class IView
-{
-public:
-	virtual void DrawLine(/*args*/) = 0;
-	virtual void DrawArc(/*args*/) = 0;
-};
-
