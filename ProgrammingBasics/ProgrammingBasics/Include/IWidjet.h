bb65b16c46067478251a94652343df311410888c
Unknown <oleshkevich19990802@mail.ru>
2018-09-30 15:27:04 +0300

    create widjet Header

--- ProgrammingBasics/ProgrammingBasics/Include/IWidjet.h
+++ ProgrammingBasics/ProgrammingBasics/Include/IWidjet.h
@@ -1,14 +1,33 @@
 #ifndef __IWIDJET
 #define __IWIDJET
 
-//class IWidjet
-//{
-//	virtual int getValue() = 0;
-//};
-//
-//class IWidjetOne : public IWidjet
-//{
-//	virtual int MyFunc() = 0;
-//};
+class Widjet
+{
+protected:
+	const string password;
+public:
+	Widjet(const string);
+};
+
+class DisplayParam : public Widjet
+{
+	Array<string> params;
+	Array<string> reqs;
+public:
+	DisplayParam(const string);
+
+	void AddParam(const Array<string>&);
+
+	void AddRequirements(const Array<string>&);
+};
+
+class CreateToolbar : public Widjet
+{
+	Array<string> possibleReqs;
+public:
+	CreateToolbar(const string);
+
+	void AddPossibleReqs(const Array<string>&);
+};
 
 #endif __IWIDJET
