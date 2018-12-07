e7bd0643e6287851ccb7943e2b7c89e515606723
Unknown <dim.meln@yandex.ru>
2018-12-07 21:25:38 +0300

    Reworked interface and added some test features

--- ProgrammingBasics/WPF_UI/WPF_UI/MainWindow.xaml.cs
+++ ProgrammingBasics/WPF_UI/WPF_UI/MainWindow.xaml.cs
@@ -18,11 +18,90 @@ namespace WPF_UI
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+    public class paramet
+    {
+        public string name;
+        public double value;
+    }
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Script_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Download_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Undo_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Reqs.Items.Add(new MenuItem
+            {
+                Header = "1"
+            });
+            Reqs.Items.Add(new MenuItem
+            {
+                Header = "2"
+            });
+            Reqs.Items.Add(new MenuItem
+            {
+                Header = "3"
+            });
+        }
+
+        private void Button_Redo_Click(object sender, RoutedEventArgs e)
+        {
+            Reqs.Items.Clear();
+            Reqs.Items.Add(new MenuItem
+            {
+                Header = "1"
+            });
+            Reqs.Items.Add(new MenuItem
+            {
+                Header = "2"
+            });
+            Reqs.Items.Add(new MenuItem
+            {
+                Header = "3"
+            });
+        }
+
+        private void Button_showParams_Click(object sender, RoutedEventArgs e)
+        {
+            DataGrid paramtable = new DataGrid();
+            paramtable.AutoGenerateColumns = true;
+            paramtable.ColumnWidth = 10;
+            List<paramet> plist = new List<paramet>
+            {
+                new paramet {name = "x1", value = 0},
+                new paramet {name = "y1", value = 1},
+                new paramet {name = "x2", value = 2},
+                new paramet {name = "y2", value = 3},
+                new paramet {name = "x1", value = 0},
+                new paramet {name = "y1", value = 1},
+                new paramet {name = "x2", value = 2},
+                new paramet {name = "y2", value = 3}
+            };
+            datagr.ItemsSource = plist;
+            //test.Children.Add(paramtable);
+        }
+
+        private void Button_showReqs_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
